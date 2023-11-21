f6ec00e134d5510e259534aee0c07672e168ac11
tarangpatil <72089368+tarangpatil@users.noreply.github.com>
2023-11-21 17:25:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ let db = [];
 
 if (localStorage.getItem("data") !== null) {
     db = JSON.parse(localStorage.getItem("data"));
-    db.forEach((i) => {
-        addData(i.name, i.email, i.dob, i.acceptTerms);
-    });
+    for (let i = db.length - 1; i >= 0; i--) {
+        addData(
+            db[i].name,
+            db[i].email,
+            db[i].password,
+            db[i].dob,
+            db[i].acceptTerms
+        );
+    }
 }
 
 document.querySelector("form").onsubmit = (e) => {
@@ -14,6 +20,7 @@ document.querySelector("form").onsubmit = (e) => {
     addData(
         formData.get("name"),
         formData.get("email"),
+        formData.get("password"),
         formData.get("dob"),
         formData.get("acceptTerms")
     );
@@ -21,6 +28,7 @@ document.querySelector("form").onsubmit = (e) => {
     db.unshift({
         name: formData.get("name"),
         email: formData.get("email"),
+        password: formData.get("password"),
         dob: formData.get("dob"),
         acceptTerms: formData.get("acceptTerms"),
     });
@@ -28,7 +36,7 @@ document.querySelector("form").onsubmit = (e) => {
     localStorage.setItem("data", JSON.stringify(db));
 };
 
-function addData(name, email, dob, termsAccepted) {
+function addData(name, email, password, dob, termsAccepted) {
     const table = document.querySelector("table");
     const newRow = table.insertRow(1);
 
@@ -36,15 +44,18 @@ function addData(name, email, dob, termsAccepted) {
     const newCell1 = newRow.insertCell(1);
     const newCell2 = newRow.insertCell(2);
     const newCell3 = newRow.insertCell(3);
+    const newCell4 = newRow.insertCell(4);
 
     newCell0.className =
         newCell1.className =
         newCell2.className =
         newCell3.className =
+        newCell4.className =
             "py-2 px-4 border-b text-center";
 
     newCell0.textContent = name;
     newCell1.textContent = email;
-    newCell2.textContent = dob;
-    newCell3.textContent = termsAccepted === "on";
+    newCell2.textContent = password;
+    newCell3.textContent = dob;
+    newCell4.textContent = termsAccepted === "on";
 }
